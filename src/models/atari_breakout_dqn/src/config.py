927f1ec3e89f3a032589e71eaeef6f7bbf4a93de
michal.nand <michal.nand@gmail.com>
2019-11-04 06:18:42 +0100

    atari

--- src/models/atari_breakout_dqn/src/config.py
+++ src/models/atari_breakout_dqn/src/config.py
@@ -7,7 +7,7 @@ class Config():
         self.gamma = 0.99
 
         self.batch_size     = 32 
-        self.learning_rate  = 0.0001
+        self.learning_rate  = 0.001
 
         self.epsilon        = common.decay.Linear(1000000, 1.0, 0.05, 0.02)
         #self.epsilon        = common.decay.Exponential(0.99999, 1.0, 0.05, 0.02)
