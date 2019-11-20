d1effea759ca406a9715b3d45bf2c459432e571d
michal.nand <michal.nand@gmail.com>
2019-11-20 05:47:02 +0100

    atari

--- src/models/atari_pong_dqn/src/model.py
+++ src/models/atari_pong_dqn/src/model.py
@@ -32,16 +32,16 @@ class Model(torch.nn.Module):
                         nn.ReLU(),
                         nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
  
-                        nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+                        nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
                         nn.ReLU(),
                         nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
             
-                        nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+                        nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
                         nn.ReLU(),
                         nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
 
                         Flatten(), 
-                        nn.Linear(fc_inputs_count*32, 256),
+                        nn.Linear(fc_inputs_count*64, 256),
                         nn.ReLU(),                      
 
                         nn.Linear(256, outputs_count)
