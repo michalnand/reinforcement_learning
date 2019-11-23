3791f9fdc36488bb70553383397514d280720efd
michal.nand <michal.nand@gmail.com>
2019-11-23 21:30:12 +0100

    atari

--- src/models/atari_pong_dqn/src/config.py
+++ src/models/atari_pong_dqn/src/config.py
@@ -9,8 +9,8 @@ class Config():
         self.batch_size     = 32 
         self.learning_rate  = 0.0002
 
-        #self.exploration    = common.decay.Linear(2000000, 1.0, 0.02, 0.02)
-        self.exploration   = common.decay.Exponential(0.999999, 1.0, 0.01, 0.01)
+        self.exploration    = common.decay.Linear(1000000, 1.0, 0.1, 0.02)
+        #self.exploration   = common.decay.Exponential(0.999999, 1.0, 0.01, 0.01)
 
         self.experience_replay_size = 10000 
  
