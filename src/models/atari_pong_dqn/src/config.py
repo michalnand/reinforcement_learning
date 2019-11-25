14a255d2a01bfd110fc265aad782fe8ff9b143c4
michal.nand <michal.nand@gmail.com>
2019-11-25 16:47:50 +0100

    atari

--- src/models/atari_pong_dqn/src/config.py
+++ src/models/atari_pong_dqn/src/config.py
@@ -6,7 +6,7 @@ class Config():
         self.type  = "dqn"
         self.gamma = 0.99
 
-        self.update_frequency = 16
+        self.update_frequency = 1
 
         self.batch_size     = 32 
         self.learning_rate  = 0.0001
