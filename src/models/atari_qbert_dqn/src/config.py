c464b32425ba07efd07ca8518e6862007c36699f
michal.nand <michal.nand@gmail.com>
2019-11-26 20:01:04 +0100

    atari

--- src/models/atari_qbert_dqn/src/config.py
+++ src/models/atari_qbert_dqn/src/config.py
@@ -6,7 +6,7 @@ class Config():
         self.type  = "dqn"
         self.gamma = 0.99
 
-        self.update_frequency = 32
+        self.update_frequency = 4
 
         self.batch_size     = 32 
         self.learning_rate  = 0.0001
@@ -14,6 +14,6 @@ class Config():
         self.exploration    = common.decay.Linear(1000000, 1.0, 0.05, 0.02)
         #self.exploration   = common.decay.Exponential(0.999999, 1.0, 0.1, 0.02)
 
-        self.experience_replay_size = 10000
+        self.experience_replay_size = 16384
  
 
