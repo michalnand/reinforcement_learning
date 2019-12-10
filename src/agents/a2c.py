1add08ca2971035a55ba8dbf1d244081e22d5811
michal.nand <michal.nand@gmail.com>
2019-12-10 18:34:55 +0100

    a2c

--- src/agents/a2c.py
+++ src/agents/a2c.py
@@ -2,7 +2,6 @@ import numpy
 import torch
 
 import agents.agent_stats
-import common.buffer_a2c
 
 from torch.distributions import Categorical
 
@@ -18,7 +17,6 @@ class Agent():
         self.observation_shape = self.env.observation_space.shape
         self.actions_count     = self.env.action_space.n
 
-        self.buffer = common.buffer_a2c.Buffer(self.batch_size)
 
         self.model      = model.Model(self.observation_shape, self.actions_count)
 
@@ -35,6 +33,12 @@ class Agent():
             self.training_stats = agents.agent_stats.AgentStats(self.save_path + "result/training")
             self.testing_stats  = agents.agent_stats.AgentStats(self.save_path + "result/testing")
 
+        self.probs_b            = []
+        self.logprobs_b         = []
+        self.state_values_b     = []
+        self.rewards_b          = []
+        self.done_b             = []
+
     def enable_training(self):
         self.enabled_training = True
 
@@ -42,26 +46,76 @@ class Agent():
         self.enabled_training = False
     
     def main(self):              
-        policy  = self.model.get_policy(self.observation)
+
+        observation_t  = torch.tensor(self.observation, dtype=torch.float32).detach().to(self.model.device).unsqueeze(0)
+        policy_t, value_t  = self.model.forward(observation_t)
 
 
-        action_probs = torch.nn.functional.softmax(torch.FloatTensor(policy))
-        action_distribution = Categorical(action_probs)
-        action = action_distribution.sample()     
+        action_probs_t        = torch.nn.functional.softmax(policy_t, dim=1)
+        action_distribution_t = torch.distributions.Categorical(action_probs_t)
+        action_t              = action_distribution_t.sample()
+       
+            
+        self.observation, reward, done, _ = self.env.step(action_t.item())
+
+
         
-        self.observation, reward, done, _ = self.env.step(action.item())
-
         round_done = done[0]
         game_done  = done[1] 
- 
 
+
+        if self.enabled_training:
+            self.probs_b.append(action_probs_t)
+            self.logprobs_b.append(action_distribution_t.log_prob(action_t))
+            self.state_values_b.append(value_t)
+            self.rewards_b.append(reward)
+            self.done_b.append(done)
+
+ 
+        if len(self.probs_b) >= self.batch_size:
+
+            dis_reward = 0
+            target_value_t = []
+            for reward in self.rewards_b[::-1]:
+                dis_reward = reward + self.gamma * dis_reward
+                target_value_t.insert(0, dis_reward)
+                
+        
+            loss = 0
+            for prob, logprob, value, target_value in zip(self.probs_b, self.logprobs_b, self.state_values_b, target_value_t):
+                advantage       = target_value  - value.item()
+                value_loss      = (target_value - value)**2 
+                action_loss     = -logprob * advantage
+                entropy_loss    = self.entropy_beta*(prob*logprob).sum()
+
+                loss += action_loss + value_loss + entropy_loss
+
+            loss = loss / len(self.probs_b)
+
+            print("LOSS = ", loss)   
+
+            loss.backward()
+
+            for param in self.model.parameters():
+                param.grad.data.clamp_(-10.0, 10.0)
+            self.optimizer.step() 
+
+
+            self.probs_b            = []
+            self.logprobs_b         = []
+            self.state_values_b     = []
+            self.rewards_b          = []
+            self.done_b             = []
+
+
+        '''
         if self.enabled_training:
             if self.buffer.is_full():
                 self.train_model()
                 self.buffer.clear()
             else:
                 self.buffer.add(self.observation, action_distribution.log_prob(action), reward, round_done, self.model.device)
-
+        '''
 
         if hasattr(self, "training_stats") and hasattr(self, "testing_stats"):
             if self.enabled_training:
@@ -84,10 +138,6 @@ class Agent():
     def load(self):
         self.model.load(self.save_path)
 
-
-    def softmax(self, x):
-        e_x = numpy.exp(x - numpy.max(x))
-        return e_x/e_x.sum()
 
 
     def compute_q_vals(self, rewards, done, device):
@@ -159,3 +209,25 @@ class Agent():
         print("loss_policy_v = ", loss_policy_v.detach().cpu().numpy())
         print("loss_entropy_v = ", loss_entropy_v.detach().cpu().numpy())
         print("\n\n")
+
+
+    def calculateLoss(self, gamma=0.99):
+        
+        # calculating discounted rewards:
+        rewards = []
+        dis_reward = 0
+        for reward in self.rewards[::-1]:
+            dis_reward = reward + gamma * dis_reward
+            rewards.insert(0, dis_reward)
+                
+        # normalizing the rewards:
+        rewards = torch.tensor(rewards)
+        rewards = (rewards - rewards.mean()) / (rewards.std())
+        
+        loss = 0
+        for logprob, value, reward in zip(self.logprobs, self.state_values, rewards):
+            advantage = reward  - value.item()
+            action_loss = -logprob * advantage
+            value_loss = F.smooth_l1_loss(value, reward)
+            loss += (action_loss + value_loss)   
+        return loss
