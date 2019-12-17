7c3164504f1af4469b93c9273b5215e60187dc6e
michal.nand <michal.nand@gmail.com>
2019-12-17 06:26:18 +0100

    common

--- src/super_mario_play.py
+++ src/super_mario_play.py
@@ -17,18 +17,19 @@ env = gym_super_mario_bros.make('SuperMarioBros-v0')
 env = common.super_mario_wrapper.Create(env, dummy_moves = 0)
 env.reset()
 
-'''
+
 model       = models.super_mario.dqn.src.model
 config      = models.super_mario.dqn.src.config.Config()
 save_path   = "./models/super_mario/dqn/"
 agent = agents.dqn.Agent(env, model, config, save_path, save_stats=False)
-'''
 
+
+'''
 model       = models.super_mario.a2c.src.model
 config      = models.super_mario.a2c.src.config.Config()
 save_path   = "./models/super_mario/a2c/"
 agent = agents.a2c.Agent(env, model, config, save_path, save_stats=False)
-
+'''
 
 agent.load()
 agent.disable_training()
