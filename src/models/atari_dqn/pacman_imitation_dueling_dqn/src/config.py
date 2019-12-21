1fdb89211f224196cffcb38240b4d7e810e2edaf
michal.nand <michal.nand@gmail.com>
2019-12-21 06:23:04 +0100

    experiments

--- src/models/atari_dqn/pacman_imitation_dueling_dqn/src/config.py
+++ src/models/atari_dqn/pacman_imitation_dueling_dqn/src/config.py
@@ -11,8 +11,8 @@ class Config():
         self.batch_size     = 32 
         self.learning_rate  = 0.0002
         
-        self.exploration    = common.decay.Linear(250000, 1.0, 0.05, 0.02)
-        self.expert_decay   = common.decay.Linear(1000000, 1.0, 0.0, 0.0)
+        self.exploration    = common.decay.Linear(1000000, 0.3, 0.05, 0.02)
+        self.expert_decay   = common.decay.Linear(500000, 1.0, 0.0, 0.0)
         
 
         self.experience_replay_size = 16384
