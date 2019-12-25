da0f6f2ab03f32cd5d9981ed67f338120f5daca6
michal.nand <michal.nand@gmail.com>
2019-12-25 16:32:30 +0100

    a2c tests

---
+++ src/models/atari_a2c/breakout_512_8/src/config.py
@@ -0,0 +1,14 @@
+class Config(): 
+
+    def __init__(self):
+        self.type           = "a2c"
+        
+        self.gamma          = 0.99
+        self.entropy_beta   = 0.01
+        self.batch_size     = 512 
+        self.bellman_steps  = 8
+
+        self.learning_rate  = 0.001
+        
+ 
+ 
