d881d1955e2e8be8bddc9911b2e13f0d0064be7c
michal.nand <michal.nand@gmail.com>
2020-01-07 06:12:02 +0100

    a2c, pong

--- src/atari_play_a2c.py
+++ src/atari_play_a2c.py
@@ -4,6 +4,7 @@ import common.atari_wrapper
 import agents.a2c
 
 
+'''
 #Pong
 import models.atari_a2c.pong.src.model
 import models.atari_a2c.pong.src.config
@@ -12,11 +13,20 @@ model  = models.atari_a2c.pong.src.model
 config = models.atari_a2c.pong.src.config.Config()
 save_path = "./models/atari_a2c/pong/"
 env = gym.make("PongNoFrameskip-v4") 
+'''
 
+
+#Breakout
+import models.atari_a2c.breakout.src.model
+import models.atari_a2c.breakout.src.config
+
+model  = models.atari_a2c.breakout.src.model
+config = models.atari_a2c.breakout.src.config.Config()
+save_path = "./models/atari_a2c/breakout/"
+env = gym.make("BreakoutNoFrameskip-v4") 
 
 
 env = common.atari_wrapper.Create(env, 96, 96, 4) 
-
 
 agent = agents.a2c.Agent(env, model, config, save_path, save_stats=False)
 agent.load()
