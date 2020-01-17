c989f8eb359a0729498730614846fe6ae3330a9b
michal.nand <michal.nand@gmail.com>
2020-01-17 18:57:03 +0100

    rainbow agent

---
+++ src/common/experience_replay_prioritized_dqn.py
@@ -0,0 +1,107 @@
+import numpy
+import collections
+import torch
+
+
+Transition = collections.namedtuple("Transition", ("observation", "q_values", "q_target_values", "action", "reward", "done"))
+
+
+class Buffer():
+
+    def __init__(self, size, gamma, bellman_steps, observation_shape, actions_count):
+        self.size   = size
+        self.gamma  = gamma
+        self.bellman_steps  = bellman_steps
+
+        self.observation_shape = observation_shape
+        self.actions_count = actions_count
+        
+        self.ptr = 0
+        self.buffer = []
+
+    def _init_zeros(self):
+        for _ in range(0, self.size):
+            observation = numpy.zeros(self.observation_shape)
+            q_values    = numpy.zeros(self.actions_count)
+            q_target_values    = numpy.zeros(self.actions_count)
+            self.buffer.append(Transition(observation, q_values, q_target_values, 0, 0.0, True))
+
+
+    def length(self):
+        return len(self.buffer)
+
+
+    def add(self, observation, q_values, action, reward, done):
+        if self.length() == 0:
+            self._init_zeros()
+
+        self.buffer[self.ptr] = Transition(observation.copy(), q_values.copy(), q_values.copy(), action, reward, done)
+        self.ptr = (self.ptr+1)%self.size
+
+    def _print(self):
+        for i in range(self.length()):
+            #print(self.buffer[i].observation, end = " ")
+            print(self.buffer[i].q_values, end = " ")
+            print(self.buffer[i].q_target_values, end = " ")
+            print(self.buffer[i].action, end = " ")
+            print(self.buffer[i].reward, end = " ")
+            print(self.buffer[i].done, end = " ")
+            print("\n")
+
+    def compute(self):
+        for n in range(self.length() - 1 - self.bellman_steps):    
+            q_target = 0.0
+
+            gamma_ = self.gamma
+            for k in range(self.bellman_steps):
+                if self.buffer[n + k].done:
+                    gamma_ = 0.0
+            
+                q_target+= self.buffer[n + k].reward*(gamma_**k)
+
+            if self.buffer[n + self.bellman_steps - 1].done:
+                gamma_ = 0.0
+            
+            gamma_ = gamma_**self.bellman_steps
+            
+            q_values    = self.buffer[n].q_values.copy()
+            action      = self.buffer[n].action 
+
+            target_q_value = q_target + gamma_*numpy.max(self.buffer[n + self.bellman_steps - 1].q_values)
+            
+            self.buffer[n].q_target_values[action] = target_q_value
+
+        self.probs = numpy.zeros(self.length())
+
+        for n in range(self.length()):
+            p = numpy.mean(((self.buffer[n].q_values - self.buffer[n].q_target_values)**2.0))
+            self.probs[n] = p
+        
+        self.probs = self.probs/numpy.sum(self.probs)
+   
+    def get_random_batch(self, batch_size, device):
+        self.compute()
+        
+        observation_shape   = self.buffer[0].observation.shape
+        state_shape         = (batch_size, ) + observation_shape[0:]
+        actions_count       = len(self.buffer[0].q_values)
+
+        q_values_shape = (batch_size, ) + (actions_count, )
+
+        input       = torch.zeros(state_shape, dtype=torch.float32).to(device)
+        target      = torch.zeros(q_values_shape, dtype=torch.float32).to(device)
+
+        indices = numpy.random.choice(self.length(), batch_size, p=self.probs)
+
+        for i in range(0, batch_size):
+            #todo - use self.probs for priority selection
+            #n      = numpy.random.randint(self.length() - 1 - self.bellman_steps)
+
+            n = indices[i]
+
+            input[i]  = torch.from_numpy(self.buffer[n].observation).to(device)
+            target[i] = torch.from_numpy(self.buffer[n].q_target_values).to(device)
+            
+        return input, target
+
+        
