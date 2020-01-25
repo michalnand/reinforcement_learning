372b0300aa5829c0f00c6bd753c25c88e9f1168d
michal.nand <michal.nand@gmail.com>
2020-01-25 17:29:31 +0100

    common

--- src/models/atari_dqn/pacman_rainbow_attention/src/config.py
+++ src/models/atari_dqn/pacman_rainbow_attention/src/config.py
@@ -9,7 +9,7 @@ class Config():
         self.update_frequency = 4
 
         self.batch_size     = 32 
-        self.learning_rate  = 0.0001 
+        self.learning_rate  = 0.00005 
         self.bellman_steps  = 4
         
         self.exploration    = common.decay.Linear(1000000, 1.0, 0.05, 0.02)
