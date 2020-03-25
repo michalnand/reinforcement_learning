877b4b1729089b268270db312209c26955284dd0
Michal Chovanec <michal@Michals-MacBook-Air.local>
2020-03-25 10:25:28 +0100

    a2c results

--- src/atari_play_a2c.py
+++ src/atari_play_a2c.py
@@ -3,17 +3,17 @@ import gym
 import common.atari_wrapper
 import agents.a2c
 
-import models.atari_a2c.breakout.src.model
-import models.atari_a2c.breakout.src.config
+import models.atari_a2c.pacman.src.model
+import models.atari_a2c.pacman.src.config
 
 
-save_path = "./models/atari_a2c/breakout/"
+save_path = "./models/atari_a2c/pacman/"
 paralel_envs_count = 1
 
 envs = [] 
 
 for _ in range(paralel_envs_count):
-    env = gym.make("BreakoutNoFrameskip-v4")
+    env = gym.make("MsPacmanNoFrameskip-v4")
     env = common.atari_wrapper.Create(env)
     env.reset()
 
@@ -22,8 +22,8 @@ for _ in range(paralel_envs_count):
 obs             = envs[0].observation_space
 actions_count   = envs[0].action_space.n
 
-model  = models.atari_a2c.breakout.src.model
-config = models.atari_a2c.breakout.src.config.Config()
+model  = models.atari_a2c.pacman.src.model
+config = models.atari_a2c.pacman.src.config.Config()
  
 agent = agents.a2c.Agent(envs, model, config, save_path, save_stats=False)
 
