13276e5e8a859407a2dad39ecad10396a1af5c39
Michal Chovanec <michal@Michals-MacBook-Air.local>
2020-04-11 14:30:06 +0200

    a2c

--- src/models/ant_a2c_continuous/src/config.py
+++ src/models/ant_a2c_continuous/src/config.py
@@ -3,10 +3,10 @@ class Config():
     def __init__(self):
         self.type           = "a2c"
         
-        self.gamma          = 0.99
+        self.gamma          = 0.95
         self.learning_rate  = 0.001
 
-        self.entropy_beta   = 0.01
+        self.entropy_beta   = 0.1
 
         self.batch_size     = 128
         
