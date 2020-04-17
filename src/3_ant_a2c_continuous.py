0faf7626c3d28416617af21685298abfe411d748
Michal Chovanec <michal@Michals-MacBook-Air.local>
2020-04-17 19:44:26 +0200

    a2c

--- src/3_ant_a2c_continuous.py
+++ src/3_ant_a2c_continuous.py
@@ -7,7 +7,7 @@ import models.ant_a2c_continuous.src.config
 
 save_path = "./models/ant_a2c_continuous/"
 
-
+'''
 paralel_envs_count = 8
 
 envs = [] 
@@ -89,5 +89,3 @@ agent.disable_training()
 
 while True:
     agent.main()
-
-'''
