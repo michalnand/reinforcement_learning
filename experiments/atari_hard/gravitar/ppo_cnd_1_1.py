e085876b56c0110b53bfd8e31271c7a7b6a043e9
michalnand <michal.nand@gmail.com>
2023-01-22 12:00:24 +0100

    experiments

---
+++ experiments/atari_hard/gravitar/ppo_cnd_1_1.py
@@ -0,0 +1,42 @@
+import time
+import torch
+import numpy
+
+import RLAgents
+
+import models.ppo_cnd_1_1.src.model_ppo        as ModelPPO
+import models.ppo_cnd_1_1.src.model_cnd_target as ModelCNDTarget
+import models.ppo_cnd_1_1.src.model_cnd        as ModelCND
+import models.ppo_cnd_1_1.src.config           as Config
+
+#torch.cuda.set_device("cuda:0")
+  
+path = "models/ppo_cnd_1_1/"   
+
+config  = Config.Config() 
+
+#config.envs_count = 1 
+ 
+envs = RLAgents.MultiEnvParallelOptimised("GravitarNoFrameskip-v4", RLAgents.WrapperMontezuma, config.envs_count)
+#envs = RLAgents.MultiEnvSeq("GravitarNoFrameskip-v4", RLAgents.WrapperMontezuma, config.envs_count)
+#envs = RLAgents.MultiEnvSeq("GravitarNoFrameskip-v4", RLAgents.WrapperMontezumaVideo, config.envs_count)
+ 
+agent = RLAgents.AgentPPOCND(envs, ModelPPO, ModelCNDTarget, ModelCND, config)
+ 
+max_iterations = 1000000  
+ 
+
+trainig = RLAgents.TrainingIterations(envs, agent, max_iterations, path, 128)
+trainig.run() 
+
+'''
+agent.load(path)
+agent.disable_training()
+
+
+while True:
+    reward, done, info = agent.main()
+
+    envs.render(0)
+    #time.sleep(0.01)
+'''
---
+++ experiments/atari_hard/pitfall/ppo_rnd_1_0.py
@@ -0,0 +1,35 @@
+import time
+
+import RLAgents
+
+import models.ppo_rnd_1_0.src.model_ppo               as ModelPPO
+import models.ppo_rnd_1_0.src.model_rnd               as ModelRND
+import models.ppo_rnd_1_0.src.config                  as Config
+
+  
+path = "models/ppo_rnd_1_0/" 
+
+config  = Config.Config() 
+ 
+#config.envs_count = 1 
+ 
+envs = RLAgents.MultiEnvParallelOptimised("PitfallNoFrameskip-v4", RLAgents.WrapperMontezuma, config.envs_count)
+#envs = RLAgents.MultiEnvSeq("PitfallNoFrameskip-v4", RLAgents.WrapperMontezuma, config.envs_count)
+ 
+agent = RLAgents.AgentPPORND(envs, ModelPPO, ModelRND, config)
+
+max_iterations = 1000000 
+ 
+trainig = RLAgents.TrainingIterations(envs, agent, max_iterations, path, 128)
+trainig.run() 
+
+'''
+agent.load(path)
+agent.disable_training()
+while True:
+    reward, done, _ = agent.main()
+
+    envs.render(0)
+    #time.sleep(0.01)
+    agent.render(0)
+'''
